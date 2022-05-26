db8abad31e8f31c4cde8f98334de3da826591557
raj161100 <rithwikrajmallam@gmail.com>
2022-05-26 11:01:44 -0400

    Add project files.

---
+++ WebApplication2/Controllers/STUDENTsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class STUDENTsController : Controller
+    {
+        private StudentEntities2 db = new StudentEntities2();
+
+        // GET: STUDENTs
+        public ActionResult Index()
+        {
+            return View(db.STUDENTs.ToList());
+        }
+
+        // GET: STUDENTs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // GET: STUDENTs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: STUDENTs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "stdid,stdname,dob,gender")] STUDENT sTUDENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.STUDENTs.Add(sTUDENT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sTUDENT);
+        }
+
+        // GET: STUDENTs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // POST: STUDENTs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "stdid,stdname,dob,gender")] STUDENT sTUDENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sTUDENT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sTUDENT);
+        }
+
+        // GET: STUDENTs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            if (sTUDENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTUDENT);
+        }
+
+        // POST: STUDENTs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            STUDENT sTUDENT = db.STUDENTs.Find(id);
+            db.STUDENTs.Remove(sTUDENT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebApplication2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplication2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03e3b6d8-331b-4ef9-aed5-cb013b2d6d5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
